aa623818b3410b0750b2348f897a9216c5995e8e
a-zamury <a.zamury@gmail.com>
2020-03-25 20:24:25 +0700

    Initial commit

---
+++ src/main/java/com/example/xmlsign/XmlsignApplication.java
@@ -0,0 +1,13 @@
+package com.example.xmlsign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlsignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmlsignApplication.class, args);
+	}
+
+}
